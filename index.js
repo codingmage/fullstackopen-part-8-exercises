99377c205dadc017e501e0e23586161716fe132a
Jean Fellipe <jeanfellipesilva93@gmail.com>
2024-01-04 20:44:34 -0300

    add exercise 8.7

--- index.js
+++ index.js
@@ -122,6 +122,10 @@ const typeDefs = `
         published: Int!
         genres: [String!]
       ) : Book
+      editAuthor(
+        name: String!
+        setBornTo: Int!
+      ) : Author
     }
 `
 
@@ -183,6 +187,17 @@ const resolvers = {
 
       books = books.concat(newBook)
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+
+      if(!author) {
+        return null
+      }
+
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
